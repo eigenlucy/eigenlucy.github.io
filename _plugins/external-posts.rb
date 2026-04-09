faace51dfe54ff7ab4207f0c6eef4652319317d7
Lucy Moglia <newuser@192.168.1.86>
2026-04-09 14:15:44 -0600

    added to library, posted stillstellung notes

--- _plugins/external-posts.rb
+++ _plugins/external-posts.rb
@@ -9,11 +9,16 @@ module ExternalPosts
 
     def generate(site)
       if site.config['external_sources'] != nil
+        exclude_titles = site.config.dig('external_sources_exclude') || []
         site.config['external_sources'].each do |src|
           p "Fetching external posts from #{src['name']}:"
           xml = HTTParty.get(src['rss_url']).body
           feed = Feedjira.parse(xml)
           feed.entries.each do |e|
+            if exclude_titles.any? { |t| e.title.downcase.include?(t.downcase) }
+              p "...skipping excluded post: #{e.title}"
+              next
+            end
             p "...fetching #{e.url}"
             slug = e.title.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
             path = site.in_source_dir("_posts/#{slug}.md")
